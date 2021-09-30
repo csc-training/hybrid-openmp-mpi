8e22efef938c5f9d51d830b0815db8ee054cbb47
Jussi Enkovaara <jussi.enkovaara@csc.fi>
2021-09-30 16:00:17 +0300

    Switch skeleton and solution

--- taskloop/solution/sum.F90
+++ taskloop/solution/sum.F90
@@ -10,15 +10,31 @@ program taskloop
 
   sum = 0.0;
 
+  !$omp parallel private(i), reduction(+:sum) 
+  !$omp single
+  !$omp taskgroup
+
+  !$omp task
   ! First sum 
+  !$omp taskloop
   do i = 1, nx
     sum = sum + vecA(i)
   end do
+  !$omp end taskloop
+  !$omp end task
 
+  !$omp task
   ! Second sum 
+  !$omp taskloop 
   do i = 1, nx
     sum = sum + vecB(i)
   end do
+  !$omp end taskloop
+  !$omp end task
+
+  !$omp end taskgroup
+  !$omp end single
+  !$omp end parallel
 
   write(*,*) 'Reduction sum', sum
 
